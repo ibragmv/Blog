49ce30520f30d0f611c447ec057a355cc5389c93
ibragmv <ibragimirpost@gmail.com>
2026-03-08 17:59:15 +0300

    Fix: Update RSS logic

--- api/feed.ts
+++ api/feed.ts
@@ -1,5 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 import type { IncomingMessage, ServerResponse } from 'http';
+import { marked } from 'marked';
 
 const supabaseUrl = process.env.VITE_SUPABASE_URL;
 const supabaseKey = process.env.VITE_SUPABASE_ANON_KEY;
@@ -33,24 +34,27 @@ export default async function handler(req: IncomingMessage, res: ServerResponse)
     
     const date = new Date().toUTCString();
 
-    const items = posts
-      ?.map((post) => {
-        const link = `${baseUrl}/blog/${post.slug}`;
-        return `
+    const items = await Promise.all(posts?.map(async (post) => {
+      const link = `${baseUrl}/blog/${post.slug}`;
+      const contentHtml = await marked.parse(post.content || '');
+      // Strip HTML for description
+      const description = contentHtml.replace(/<[^>]*>?/gm, '').slice(0, 200) + '...';
+
+      return `
     <item>
       <title><![CDATA[${post.title}]]></title>
       <link>${link}</link>
-      <guid isPermaLink="true">${link}</guid>
+      <guid isPermaLink="false">${post.id}</guid>
       <pubDate>${new Date(post.created_at).toUTCString()}</pubDate>
-      <description><![CDATA[${post.content ? post.content.slice(0, 200).replace(/#/g, '') + '...' : ''}]]></description>
-      <author>ibragimirpost@gmail.com (Ibragim Ibragimov)</author>
+      <description><![CDATA[${description}]]></description>
+      <content:encoded><![CDATA[${contentHtml}]]></content:encoded>
+      <dc:creator>Ibragim Ibragimov</dc:creator>
     </item>`;
-      })
-      .join('');
+    }) || []);
 
     const rss = `<?xml version="1.0" encoding="UTF-8" ?>
 <?xml-stylesheet type="text/xsl" href="/rss.xsl"?>
-<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:content="http://purl.org/rss/1.0/modules/content/">
   <channel>
     <title>Ibragim Ibragimov</title>
     <link>${baseUrl}</link>
@@ -62,9 +66,8 @@ export default async function handler(req: IncomingMessage, res: ServerResponse)
       <title>Ibragim Ibragimov</title>
       <link>${baseUrl}</link>
     </image>
-    <atom:icon>${baseUrl}/favicon.ico</atom:icon>
     <atom:link href="${baseUrl}/feed.xml" rel="self" type="application/rss+xml" />
-    ${items}
+    ${items.join('')}
   </channel>
 </rss>`;
 
